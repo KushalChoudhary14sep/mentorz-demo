304bcfe44aeacf66174e19f7646f017bb693c851
kushal14sep <kushal14sep@gmail.com>
2021-02-20 17:23:00 +0530

    added gitignore

---
+++ mentroz-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mentroz-demo
+//
+//  Created by Kushal Choudhary on 20/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
